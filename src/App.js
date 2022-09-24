701ccded74320714953dbdb86698048734561d12
Mateus Cruz <fdsmeuirmao2@outlook.com>
2022-09-24 05:00:01 -0300

    Escolhe aleatoriamente uma palavra,esconde a palavra e a popula

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
+import { useState } from "react";
 import forcaInicial from "./images/assets/forca0.png"
+import PalavrasArray from "./palavras"
 
 
 export default function App(){
 
+    function ClicouLetra(letraSelecionada){
+        setDesabilitado(false)
+        letrasClicadas.push(letraSelecionada)
+        console.log(letrasClicadas)
+    }
+ 
+    
+let numerosDeLetras;
+    const [Secreto, setSecreto] = useState("Bem vindo ao jogo da forca!")
+
+    function EscolherPalavra(){
+        const palavraAleatoria = PalavrasArray[Math.floor(Math.random()*PalavrasArray.length)];
+        const ArrayLetras = palavraAleatoria.split("");
+        console.log(ArrayLetras);
+        setDesabilitado(false);
+        setPrincipal(true);
+        numerosDeLetras = ArrayLetras.length;
+        Number(numerosDeLetras);
+        teste();
+
+        function teste(){
+            const palavraAberta = [...ArrayLetras];
+            const palavraEscondida = palavraAberta.fill("_")
+            setSecreto(palavraEscondida)
+        }
+
+
+
+    }
+    
+        
+
+
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
    
+    const letrasClicadas = ["b"]
+    const [Desabilitado, setDesabilitado] = useState(true)
+    const [DesabilitadoBotaoPrincial, setPrincipal] = useState(false)
+
+
+
     return(
         <>
         <div className="ConteinerJogo">
            <img src={forcaInicial} alt="inicialforca" className="forca"/>
            <div className="Lateral">
-           <button>Escolha Palavra</button>
-            <p>Palavra</p>
+           <button onClick={EscolherPalavra} disabled={DesabilitadoBotaoPrincial}>Escolha Palavra</button>
+            <p>{Secreto}</p>
            </div>
         </div>
         <div className="Conteiner-Respostas">
             <ul className="Conteiner-letras">
                 {alfabeto.map((l, index) => 
                 <li>
-                        <button key={index}>{l}</button>
+                        <button  className="botaoLetra" key={index}  onClick={() => ClicouLetra(l)} disabled={Desabilitado}>{l}</button>
                     </li>
                     )}
             </ul>
-                
+                       
             <div className="Responder-Resposta">
                 <p>Já Sei a pergunta!</p>
                 <input type="text" className="Chute"></input>
