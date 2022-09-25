d5d4063db8251778d55cd20b1ca5e8fc4fb8c362
Mateus Cruz <fdsmeuirmao2@outlook.com>
2022-09-25 03:42:25 -0300

    homemzinho sendo alterado a cada erro

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import { useState } from "react";
-import forcaInicial from "./images/assets/forca0.png"
+import forca0 from "./images/assets/forca0.png"
+import forca1 from "./images/assets/forca1.png"
+import forca2 from "./images/assets/forca2.png"
+import forca3 from "./images/assets/forca3.png"
+import forca4 from "./images/assets/forca4.png"
+import forca5 from "./images/assets/forca5.png"
+import forca6 from "./images/assets/forca6.png"
+
 import PalavrasArray from "./palavras"
 
+const Forcas = [forca0,forca1,forca2,forca3,forca4,forca5,forca6]
 
 
 export default function App(){
+
+    const[Contador, setContador] = useState(1)
+    const[Imagem, setImagem] = useState(Forcas[0])
+
+
 
     function ClicouLetra(letraSelecionada){
         setDesabilitado(false)
         const novaLista = [...letrasClicadas,letraSelecionada]
         setClicado(novaLista)
         console.log(letraSelecionada)
+
+        if(Aleatorio.includes(letraSelecionada)){
+            console.log("TEM")
+            console.log(Contador)
+        } else{
+            console.log("NAO TEM")
+            setContador(Contador + 1)
+            setImagem(Forcas[Contador])
+        }
     }
  
     
@@ -18,11 +40,14 @@ let numerosDeLetras;
 
     const [PalavraCerta, setPalavra] = useState("")
     const [Secreto, setSecreto] = useState("Bem vindo ao jogo da forca!")
+    const [Aleatorio, setAletorio] = useState()
+    
 
     function EscolherPalavra(){
         const palavraAleatoria = PalavrasArray[Math.floor(Math.random()*PalavrasArray.length)];
         setPalavra(palavraAleatoria)
         const ArrayLetras = palavraAleatoria.split("");
+        setAletorio(ArrayLetras)
         console.log(ArrayLetras);
         setDesabilitado(false);
         setPrincipal(true);
@@ -59,7 +84,7 @@ let numerosDeLetras;
     return(
         <>
         <div className="ConteinerJogo">
-           <img src={forcaInicial} alt="inicialforca" className="forca"/>
+           <img src={Imagem} alt="inicialforca" className="forca"/>
            <div className="Lateral">
            <button onClick={EscolherPalavra} disabled={DesabilitadoBotaoPrincial}>Escolha Palavra</button>
             <p className={cor}>{Secreto}</p>
